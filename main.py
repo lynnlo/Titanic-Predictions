9e150a1aadf26528e1526bd87b14bdce9d61551f
Lynn Ong <tharlynnhtet.ong@gmail.com>
2021-01-10 16:07:26 -0700

    Finalized Model, Added Tests.

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-
 # Imports
 import numpy as np
-from numpy.core.numeric import NaN
 import pandas as pd
 
 from sklearn.preprocessing import MinMaxScaler
 
-from tensorflow.keras import Model, Sequential
+from tensorflow.keras import Model
+from tensorflow.keras.models import load_model
 from tensorflow.keras.layers import Input as KInput, Dense as KDense
-
-from matplotlib import pyplot as plt
 
 # Load data
 data = pd.read_csv("./train.csv")
 
 # Preprocess
-trainable = ["Pclass", "Age", "Fare"]
+trainable = ["Pclass", "Age", "Parch", "Fare"]
 passengers = []
 survived = []
 scalers = []
 
 for i in trainable:
-	scaler = MinMaxScaler(feature_range=(0,1))
+	scaler = MinMaxScaler(feature_range=(0, 1))
 	scaler.fit(np.array(data[i]).reshape(-1, 1))
 	scalers.append(scaler)
 
@@ -31,24 +28,61 @@ for i in range(len(data)):
 	for ti,t in enumerate(trainable):
 		value = np.array(np.float(data[t][i])).reshape(1, -1) if not np.isnan(data[t][i]) else [[0]]
 		passenger.append(scalers[ti].transform(value)[0][0])
+	passenger.append(1 if data["Sex"][i] == "male" else 0) # Gender
 	passengers.append(passenger)
 	survived.append(np.float(data["Survived"][i]))
 
 passengers = np.array(passengers) # Values
 survived = np.array(survived) # Labels
 
-print(passengers)
-
 # Model
-model_input = KInput(3) # Input
-model_layer_0 = KDense(64, activation="relu")(model_input) # Hidden
-model_output = KDense(1, activation="sigmoid")(model_layer_0) # Output
+new = True
 
-model = Model(inputs=model_input, outputs=model_output)
-model.compile(optimizer='adam', loss='binary_crossentropy')
+if new:
+	model_input = KInput(len(trainable)+1) # Input
+	model_layer_0 = KDense(64, activation="relu")(model_input) # Hidden
+	model_output = KDense(1, activation="sigmoid")(model_layer_0) # Output
 
-model.fit(x=passengers, y=survived, batch_size=10, epochs=20)
-model.summary()
+	model = Model(inputs=model_input, outputs=model_output)
+	model.compile(optimizer='adam', loss='binary_crossentropy')
 
-print(model.predict(passengers[0].reshape(1, 3)))
-print(survived[0])
+	model.fit(x=passengers, y=survived, batch_size=10, epochs=100) # Training
+	model.summary()
+
+	model.save("model")
+else:
+	model = load_model("model")
+
+def predict(data : np.array):
+	return np.round(model.predict(data.reshape(1, len(trainable)+1)))[0][0]
+
+# Prediction
+testing_data = pd.read_csv("./test.csv")
+
+testing_passengers = []
+testing_id = []
+testing_survived = []
+testing_results = []
+
+for i in range(len(testing_data)):
+	passenger = []
+	# Scale per value
+	for ti,t in enumerate(trainable):
+		value = np.array(np.float(testing_data[t][i])).reshape(1, -1) if not np.isnan(testing_data[t][i]) else [[0]]
+		passenger.append(scalers[ti].transform(value)[0][0])
+	passenger.append(1 if testing_data["Sex"][i] == "male" else 0) # Gender
+	testing_passengers.append(passenger)
+	testing_id.append(testing_data["PassengerId"][i])
+
+testing_passengers = np.array(testing_passengers)
+testing_id = np.array(testing_id)
+
+for i,v in enumerate(testing_passengers):
+	testing_survived.append(predict(v))
+	testing_results.append([testing_id[i], testing_survived[i]])
+
+testing_survived = np.array(testing_survived, dtype=int)
+testing_results = np.array(testing_results, dtype=int)
+
+# To CSV
+pd.DataFrame(testing_results).to_csv("results.csv", index=None, header=["PassengerId", "Survived"])
