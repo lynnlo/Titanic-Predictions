549d57f7c897a4dbd8ef4bdcd0e95bb08af82abf
Lynn Ong <tharlynnhtet.ong@gmail.com>
2021-01-10 01:36:55 -0700

    Added Model

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 
+from tensorflow.keras import Model, Sequential
+from tensorflow.keras.layers import Input as KInput, Dense as KDense
+
 from matplotlib import pyplot as plt
 
 # Load data
@@ -14,8 +17,22 @@ survived = []
 for i in range(len(data)):
 	passenger = []
 	for t in trainable:
-		passenger.append(data[t][i])
+		passenger.append(np.float(data[t][i]))
 	passengers.append(passenger)
-	survived.append(data["Survived"][i])
+	survived.append(np.float(data["Survived"][i]))
 
-print(survived)
+passengers = np.array(passengers)
+survived = np.array(survived)
+
+# Model
+model_input = KInput(3)
+model_layer_0 = KDense(64, activation="relu")(model_input)
+model_output = KDense(1, activation="sigmoid")(model_layer_0)
+
+model = Model(inputs=model_input, outputs=model_output)
+model.compile(optimizer='adam', loss='binary_crossentropy')
+
+model.fit(x=passengers, y=survived, batch_size=10, epochs=5)
+model.summary()
+ 
+print(model.predict([[2, 64, 154]]))
